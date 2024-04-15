772ccf28e4d80af912be15e905d30ed781a3aa73
PiotrEsse <42764623+PiotrEsse@users.noreply.github.com>
2024-04-15 15:54:29 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@ IMAP_SERVER = 'your_imap_server_address'
 IMAP_USERNAME = 'your_imap_username'
 IMAP_PASSWORD = 'your_imap_password'
 
-PMG_API_URL = 'your_pmg_api_url'
+PMG_API_URL = 'your_pmg_api_url:8006/api2/json'
 PMG_USERNAME = 'your_pmg_username'
 PMG_PASSWORD = 'your_pmg_password'
 
